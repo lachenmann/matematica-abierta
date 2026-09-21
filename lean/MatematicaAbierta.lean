3f615610508c669fcdbbabcf48c6cd75ac8cb16f
lachenmann <80990661+lachenmann@users.noreply.github.com>
2026-09-21 01:44:14 -0300

    Importar certificados racionales O3 en biblioteca formal

--- lean/MatematicaAbierta.lean
+++ lean/MatematicaAbierta.lean
@@ -14,6 +14,7 @@ import MatematicaAbierta.Continuo.SimulacionAcotada
 import MatematicaAbierta.Continuo.PrimerExito
 import MatematicaAbierta.Continuo.ComputabilidadAproximante
 import MatematicaAbierta.Continuo.NombresRaiz
+import MatematicaAbierta.Continuo.DecididoresCortes
 
 /-!
 # Matemática Abierta: biblioteca formal
